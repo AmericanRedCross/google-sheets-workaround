f833682c727de368eb303b014e07e8c33952af77
danbjoseph <danbjoseph@gmail.com>
2020-12-01 08:18:56 -0500

    add url for gdpc app map

--- settings.js
+++ settings.js
@@ -1,7 +1,9 @@
 var settings = {}
 
 settings.urls = [
-	{ name: 'research-mapping',
+	{ name: 'gdpc-movement-apps',
+		url: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRl_O5oRaCB-YGQtqDKH0Hm9GWxH6T4qGUfPe9oM9HDNXtEaPacIFeztQfpPA72IyVGEVwzXZjQYiEW/pub?gid=0&single=true&output=csv'}
+	,{ name: 'research-mapping',
     url: 'https://docs.google.com/spreadsheets/d/1NgKu_88w4Im7fWeosuRdzLeIg1qG26q0x70RYULdew0/pub?output=csv'}
   ,{ name: 'gdpc-app-activities-map',
     url: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTMa5fAK3XpxuUrA4RH2L7GFTr1k633g3Cq8ChZOPKBSHFGb8iEY9U_6n56Lcs6McvPImE_GMsMRWPQ/pub?gid=1052010868&single=true&output=csv'}
